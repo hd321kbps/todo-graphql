86060d5e94e3dafa3debf8376231f7819bbe5ad5
hd321kbps <hd321kbps@yandex.ru>
2021-07-16 00:15:57 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,39 @@
+﻿const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const path = require('path');
+const sequelize = require('./utils/database');
+const schema = require('./graphql/schema');
+const resolver = require('./graphql/resolver');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+
+app.use(
+  graphqlHTTP({
+    schema,
+    rootValue: resolver,
+    graphiql: true
+  })
+);
+
+app.use((req, res, next) => {
+  res.sendFile('/index.html');
+});
+
+async function start() {
+  try {
+    await sequelize.sync();
+
+    app.listen(PORT, () => {
+      console.log(`Server started http://localhost:${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
---
+++ utils/database.js
@@ -0,0 +1,15 @@
+﻿const process = require('process');
+const Sequelize = require('sequelize');
+
+const DB_NAME = process.env.DB_NAME || 'node-todo';
+const USERNAME = process.env.DB_USERNAME || 'root';
+const PASSWORD = process.env.DB_PASSWORD || '';
+
+const sequelize = new Sequelize(DB_NAME, USERNAME, PASSWORD, {
+  host: 'localhost',
+  dialect: 'mariadb',
+  // Показывать ли SQL запросы
+  logging: false
+});
+
+module.exports = sequelize;
